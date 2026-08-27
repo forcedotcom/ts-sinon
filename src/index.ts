28e88de405874963b478b5879d4c78c826c6d7c7
Katy Bowman <katy.bowman@salesforce.com>
2026-08-27 16:36:51 -0400

    build: migrate to TypeScript 6 and ESLint flat config
    Bump typescript to ^6, dev-scripts to ^14, and adopt the ESLint
    flat-config format (eslint.config.mjs), replacing the
    legacy .eslintrc.cjs files. Removes now-stale eslint-disable comments and adds new where needed.

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,6 @@ const makeProxyGet = (sandbox: SinonSandbox, members: OpenDictionary, stubMissin
   return (target: OpenDictionary, name: string): any => {
     const stubMemberFn = (fn: OpenFunction): Stub<object & AnyFunction<any>> => {
       if (Object.keys(fn).length > 0) {
-        // eslint-disable-next-line @typescript-eslint/no-use-before-define
         return stubCallable(sandbox, fn, fn);
       }
       return sandbox.stub().callsFake(fn);
@@ -43,13 +42,11 @@ const makeProxyGet = (sandbox: SinonSandbox, members: OpenDictionary, stubMissin
     }
     if (members[name] != null) {
       if (isFunction(members[name])) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
         cache[name] = stubMemberFn(members[name]);
       } else {
         cache[name] = members[name];
       }
     } else if (isFunction(target[name])) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
       cache[name] = stubMemberFn(target[name]);
     } else if (target[name] == null && stubMissing) {
       cache[name] = sandbox.stub();
@@ -193,5 +190,6 @@ export function fromStub<T extends object>(stubbed: StubbedType<T>): T {
   // Since `stubObject` produces a `Proxy` of `T` backed by a real instance of `T`, it's safe to simply achieve this
   // with a type assertion.  Since much of the point of this library is to help avoid the need for type assertions,
   // prefer the use of using `fromStub` over the direct assertion.
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
   return stubbed as T;
 }
